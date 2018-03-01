5115e64b675adb7d4a41fae7adc9b6d64a3a959d
humkeym <mhhplumber@users.noreply.github.com>
2018-03-01 01:22:15 -0800

    model functional

--- main.js
+++ main.js
@@ -4,18 +4,56 @@ function fillCircle(ctx, x, y, radius){
   ctx.fill();
 }
 
-function Spawner(game, x, y) {
-  this.radius = 30;
-  this.life = 800;
-  this.ctr = 0;
-  Entity.call(this, game, x, y);
+function distance(a, b) {
+    var dx = a.x - b.x;
+    var dy = a.y - b.y;
+    return Math.sqrt(dx * dx + dy * dy);
 }
 
-Spawner.prototype = new Entity();
+function Circle(game, x, y, radius, lspan) {
+    this.radius = radius;
+    this.life = lspan;
+    this.colors = ["Red", "Green", "Blue", "White"];
+    Entity.call(this, game, x, y);
+}
+
+Circle.prototype = new Entity();
+Circle.prototype.constructor = Circle;
+
+Circle.prototype.collide = function (other) {
+    return distance(this, other) < this.radius + other.radius;
+};
+
+Circle.prototype.collideLeft = function () {
+    return (this.x - this.radius) < 0;
+};
+
+Circle.prototype.collideRight = function () {
+    return (this.x + this.radius) > window.innerWidth;
+};
+
+Circle.prototype.collideTop = function () {
+    return (this.y - this.radius) < 0;
+};
+
+Circle.prototype.collideBottom = function () {
+    return (this.y + this.radius) > window.innerHeight;
+};
+
+Circle.prototype.update = function () {
+    Entity.prototype.update.call(this);
+};
+
+function Spawner(game, x, y) {
+  this.ctr = 0;
+  Circle.call(this, game, x, y, 15, 200);
+}
+
+Spawner.prototype = new Circle();
 Spawner.prototype.constructor = Spawner;
 
 Spawner.prototype.update = function () {
-  if (this.ctr++ === 200){
+  if (this.ctr++ === 50){
     this.ctr = -1;
     var nspawn = 5 + Math.ceil(10 * Math.random());
     for (i = 0; i <= nspawn; i++){
@@ -25,7 +63,7 @@ Spawner.prototype.update = function () {
       gameEngine.addEntity(new Spore(gameEngine, x, y));
     }
   }
-  if(!this.life--){
+  if(!this.life-- || this.lspan > 200){
     this.removeFromWorld = true;
   }
 }
@@ -37,25 +75,91 @@ Spawner.prototype.draw = function (ctx) {
 }
 
 function Spore(game, x, y) {
-  this.speed = 30;
-  this.radius = 10;
-  this.life = 50 + Math.ceil(Math.random() * 200);
-  Entity.call(this, game, x, y);
+  this.velocity = {x: (Math.random()-0.5)*240, y: (Math.random()-0.5)*240};
+  Circle.call(this, game, x, y, 5, 25 + Math.ceil(Math.random() * 50));
 }
 
-Spore.prototype = new Entity();
+Spore.prototype = new Circle();
 Spore.prototype.constructor = Spawner;
 
 Spore.prototype.update = function () {
-  this.x += (Math.random()-0.5)*this.speed;
-  this.y += (Math.random()-0.5)*this.speed;
+  // this.x += (Math.random()-0.5)*this.speed;
+  // this.y += (Math.random()-0.5)*this.speed;
   if(!this.life--){
     this.removeFromWorld = true;
-    if (Math.random() < 0.06){
+    if (Math.random() < 0.05){
       var that = this;
       gameEngine.addEntity(new Spawner(gameEngine, that.x, that.y));
     }
   }
+
+  this.x += this.velocity.x * this.game.clockTick;
+  this.y += this.velocity.y * this.game.clockTick;
+
+    if (this.collideLeft() || this.collideRight()) {
+        this.velocity.x = -this.velocity.x;
+        if (this.collideLeft()) this.x = this.radius;
+        if (this.collideRight()) this.x = window.innerWidth - this.radius;
+        this.x += this.velocity.x * this.game.clockTick;
+        this.y += this.velocity.y * this.game.clockTick;
+    }
+
+    if (this.collideTop() || this.collideBottom()) {
+        this.velocity.y = -this.velocity.y;
+        if (this.collideTop()) this.y = this.radius;
+        if (this.collideBottom()) this.y = window.innerHeight - this.radius;
+        this.x += this.velocity.x * this.game.clockTick;
+        this.y += this.velocity.y * this.game.clockTick;
+    }
+
+    for (var i = 0; i < this.game.entities.length; i++) {
+        var ent = this.game.entities[i];
+        if (ent instanceof Spawner){
+          if (this.collide(ent)){
+            this.removeFromWorld = true;
+            ent.life -= this.life;
+          } else {
+            //repell somehow
+          }
+        } else if (ent !== this && this.collide(ent)) {
+            var temp = { x: this.velocity.x, y: this.velocity.y };
+
+            var dist = distance(this, ent);
+            var delta = this.radius + ent.radius - dist;
+            var difX = (this.x - ent.x)/dist;
+            var difY = (this.y - ent.y)/dist;
+
+            this.x += difX * delta / 2;
+            this.y += difY * delta / 2;
+            ent.x -= difX * delta / 2;
+            ent.y -= difY * delta / 2;
+
+            this.velocity.x = ent.velocity.x;
+            this.velocity.y = ent.velocity.y;
+            ent.velocity.x = temp.x;
+            ent.velocity.y = temp.y;
+            this.x += this.velocity.x * this.game.clockTick;
+            this.y += this.velocity.y * this.game.clockTick;
+            ent.x += ent.velocity.x * this.game.clockTick;
+            ent.y += ent.velocity.y * this.game.clockTick;
+        }
+
+        if (ent != this && this.collide({ x: ent.x, y: ent.y, radius: this.visualRadius })) {
+            var dist = distance(this, ent);
+            if (this.it && dist > this.radius + ent.radius + 10) {
+                var difX = (ent.x - this.x)/dist;
+                var difY = (ent.y - this.y)/dist;
+                this.velocity.x += difX * acceleration / (dist*dist);
+                this.velocity.y += difY * acceleration / (dist * dist);
+                var speed = Math.sqrt(this.velocity.x*this.velocity.x + this.velocity.y*this.velocity.y);
+                if (speed > maxSpeed) {
+                    var ratio = maxSpeed / speed;
+                    this.velocity.x *= ratio;
+                    this.velocity.y *= ratio;
+                }
+            }
+        }
+    }
 }
 
 Spore.prototype.draw = function (ctx) {
